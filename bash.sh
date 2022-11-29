93c1e774478c4020f07906c391551f4b02410474
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-11-29 21:26:54 +0530

    changes

--- bash.sh
+++ bash.sh
@@ -1,4 +1,6 @@
 echo "starting Bot ~@DroneBots";
+echo $PORT
+kill -9 $(lsof -t -i:$PORT)
 nohup python3 webApp.py > save_pid.txt >&1 &
 # lsof -t -i:8082   
 # kill -9 $(lsof -t -i:8082)
--- webApp.py
+++ webApp.py
@@ -1,5 +1,7 @@
-from gunicorn.app.wsgiapp import WSGIApplication
 from flask import Flask
+from decouple import config
+
+PORT = config("PORT", default=None)
 
 # Function that create the app
 
@@ -25,33 +27,9 @@ def startWeb():
     APP.run(debug=False)
 
 
-class StandaloneApplication(WSGIApplication):
-    def __init__(self, app_uri, options=None):
-        self.options = options or {}
-        self.app_uri = app_uri
-        super().__init__()
-
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-
-def run():
-    options = {
-        "bind": "0.0.0.0:8082",
-        "workers": 1
-    }
-    StandaloneApplication("webApp:create_app()", options).run()
-
-
 if __name__ == "__main__":
-    run()
-#     p = Process(target=startWeb)
-#     p.start()
-#     # bot.run_until_disconnected()
-#     # APP.run(host='0.0.0.0', port=5000, debug=True)
+    # run()
+    #     p = Process(target=startWeb)
+    #     p.start()
+    #     # bot.run_until_disconnected()
+    APP.run(host='0.0.0.0', port=PORT, debug=False)
