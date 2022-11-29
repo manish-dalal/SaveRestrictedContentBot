417a764b71e37ac64da34be3bf008cc0415b5173
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-11-29 21:40:31 +0530

    changes

--- webApp.py
+++ webApp.py
@@ -1,3 +1,4 @@
+from gunicorn.app.wsgiapp import WSGIApplication
 from flask import Flask
 from decouple import config
 
@@ -27,9 +28,34 @@ def startWeb():
     APP.run(debug=False)
 
 
+class StandaloneApplication(WSGIApplication):
+    def __init__(self, app_uri, options=None):
+        self.options = options or {}
+        self.app_uri = app_uri
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+
+def run():
+    url = "0.0.0.0:" + PORT
+    options = {
+        "bind": url,
+        "workers": 1
+    }
+    StandaloneApplication("webApp:create_app()", options).run()
+
+
 if __name__ == "__main__":
-    # run()
-    #     p = Process(target=startWeb)
-    #     p.start()
-    #     # bot.run_until_disconnected()
-    APP.run(host='0.0.0.0', port=PORT, debug=False)
+    run()
+#     p = Process(target=startWeb)
+#     p.start()
+#     # bot.run_until_disconnected()
+#     # APP.run(host='0.0.0.0', port=5000, debug=True)
