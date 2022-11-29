720c0246f50c2deec3ac18b8bb9f916adab49179
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-11-29 21:17:19 +0530

    chsngr

--- bash.sh
+++ bash.sh
@@ -1,2 +1,5 @@
 echo "starting Bot ~@DroneBots";
+nohup python3 webApp.py > save_pid.txt >&1 &
+# lsof -t -i:8082   
+# kill -9 $(lsof -t -i:8082)
 python3 -m main
---
+++ webApp.py
@@ -0,0 +1,57 @@
+from gunicorn.app.wsgiapp import WSGIApplication
+from flask import Flask
+
+# Function that create the app
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+
+    # Simple route
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    return app  # do not forget to return the app
+
+
+APP = create_app()
+
+
+def startWeb():
+    print('Hello start web')
+    # APP.run(host='0.0.0.0', port=5000, debug=True)
+    APP.run(debug=False)
+
+
+class StandaloneApplication(WSGIApplication):
+    def __init__(self, app_uri, options=None):
+        self.options = options or {}
+        self.app_uri = app_uri
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+
+def run():
+    options = {
+        "bind": "0.0.0.0:8082",
+        "workers": 1
+    }
+    StandaloneApplication("webApp:create_app()", options).run()
+
+
+if __name__ == "__main__":
+    run()
+#     p = Process(target=startWeb)
+#     p.start()
+#     # bot.run_until_disconnected()
+#     # APP.run(host='0.0.0.0', port=5000, debug=True)
